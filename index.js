dfc9c795c03d481b0fa1eaf1e6c3e77483a4f783
abdullah <abdullah.karkarli@ogr.iu.edu.tr>
2023-03-20 22:14:36 +0300

    first function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+const employee={
+    name: Abdullah,
+    streetAddress: Bahelievler,
+};
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    return {
+        ...obj,
+        [key]:value,
+    };
+}
