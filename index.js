4c619bff767de2349fe6a1f5ccbd6f4da39f829b
abdullah <abdullah.karkarli@ogr.iu.edu.tr>
2023-03-20 22:23:47 +0300

    finalalizing

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const employee={
     name: Abdullah,
     streetAddress: Bahelievler,
 };
-function updateEmployeeWithKeyAndValue(obj,key,value){
+function updateEmployeeWithKeyAndValue(employee,key,value){
     return {
-        ...obj,
+        ...employee,
         [key]:value,
     };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    return employee.key=value;
+}
+function deleteFromEmployeeByKey(employee,key){
+    let temp= {...employee} ;
+    return delete temp.key;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    return delete employee.key;
 }
